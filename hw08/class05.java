cb89a9ce47936cfe0b301021d0d5ef532abd9ac4
rwrewrwer <91109259+rwrewrwer@users.noreply.github.com>
2021-10-13 19:45:50 +0800

    Update class05.java

--- hw08/class05.java
+++ hw08/class05.java
@@ -1,1 +1,48 @@
 
+class CBox {
+	int length;
+	int width;
+	int heigth;
+	int volume()
+	{
+		return length*width*heigth ;
+	}
+	int surfaceArea() 
+	{
+		return (2*(width*width))+(4*(length*heigth)) ;
+	}
+	void showData()
+	{
+		System.out.println("length= "+length);
+		System.out.println("width= "+width);
+		System.out.println("heigth= "+heigth);
+	}
+	void showAll()
+	{
+		System.out.println("length= "+length);
+		System.out.println("width= "+width);
+		System.out.println("heigth= "+heigth);
+		System.out.println("volume= "+volume());
+		System.out.println("surfceArea= "+surfaceArea());
+		
+	}
+}
+
+
+public class hw8_5 {
+
+	public static void main(String[] args) {
+
+		CBox box ;
+		box = new CBox();
+		
+		box.length = 1;
+		box.width = 1;
+		box.heigth = 1;
+		
+		
+		box.showData() ;
+		box.showAll();
+	}
+
+}
