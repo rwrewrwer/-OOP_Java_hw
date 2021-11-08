423613491856ad11d7aa912df49173ec6ba41dd8
rwrewrwer <91109259+rwrewrwer@users.noreply.github.com>
2021-11-08 23:18:17 +0800

    Update class06.java

--- hw9/class06.java
+++ hw9/class06.java
@@ -1,18 +1,18 @@
- class add2n{
-	 int sum = 0;
-	 public add2n(int n) {
+  class add2n{
+
+
+	 public static void add2n(int n) {
+		int sum = 0;
 		 for(int i=0;i<=n;i++) {
 			 sum = sum+i;
+			
 		 }
-		 
+		  System.out.println(sum);
 	 }
-	 public void show() {System.out.println(sum);}
  }
 public class hw9_6{
 	public static void main(String argv[]) {
-		add2n ad = new add2n(5);
-		ad.show();
-		add2n ad1 = new add2n(10);
-		ad1.show();
+		add2n.add2n(5);
+		add2n.add2n(10);
 	}
 }
