feb9636c748b28c7e0b6232188a18aec9ae591c0
rwrewrwer <91109259+rwrewrwer@users.noreply.github.com>
2021-11-08 23:13:14 +0800

    Update class07.java

--- hw9/class07.java
+++ hw9/class07.java
@@ -1,19 +1,20 @@
 class power{
-	int sum ;
-	public power(int x,int n) {
+	static int sum ;
+	public static void power(int x,int n) {
 		sum = x;
 		for(int i=1;i<n;i++) {
 			sum = sum*x ;
 		}
 	}
-	public void show() {System.out.println(sum);}
+	public static void show() {System.out.println(sum);}
 }
 
 public class hw9_7 {
 	public static void main (String argv[]) {
-		power p = new power(2,5);
-		p.show();
-		power p1 = new power(3,2);
-		p1.show();
+		power.power(2,5);
+		power.show();
+		power.power(3,2);
+		power.show();
 	}
 }
+
