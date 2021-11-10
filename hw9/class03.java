efe242553833d8a93b477cb3024bc8a1eb08aa69
rwrewrwer <91109259+rwrewrwer@users.noreply.github.com>
2021-11-10 23:40:47 +0800

    Update class03.java

--- hw9/class03.java
+++ hw9/class03.java
@@ -1,1 +1,24 @@
 
+  class Caaa{
+	
+	public int value;
+	public Caaa() {
+		value = 10;
+		System.out.println("value = "+value);
+	}
+	public Caaa(int i) {
+		value = i;
+		System.out.println("value = "+value);
+	}
+}
+public class hw9_3
+{
+   public static void main(String args[])
+   {
+	   Caaa obj1 = new Caaa();//呼叫第5行的Caaa()
+	   Caaa obj2 = new Caaa(12); //呼叫第9行Caaa(int i)
+	   
+   }
+}
+//(c) 不行 在開發平台上顯示錯誤
+//(d) 可以
