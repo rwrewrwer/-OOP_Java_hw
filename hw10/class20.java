31e8d8a1bc3d61b2cd7fe09e8a37d55d7b8cc5a1
rwrewrwer <91109259+rwrewrwer@users.noreply.github.com>
2021-11-14 22:01:48 +0800

    Create class20.java

---
+++ hw10/class20.java
@@ -0,0 +1,37 @@
+class CRectangle
+{
+	protected int length;
+	protected int width;
+	public CRectangle(int l,int w)
+	{
+		length = l;
+		width = w;		
+	}
+	public String toString()
+	{
+		String str = "length = "+length+", width = "+width+", area = "+(length*width);
+		return str;
+	}
+	public String toString(int a,int b)
+	{
+		if(a>=b)
+		
+			return super.toString();
+	
+		else
+		
+			return "Argument Error";
+		
+	}
+	
+}
+public class hw10_1
+{
+	public static void main(String argv[])
+	{
+		CRectangle rect = new CRectangle(6,2);
+		System.out.println(rect.toString());
+		System.out.println(rect.toString(6,2));
+		System.out.println(rect.toString(3,5));
+	}
+}
