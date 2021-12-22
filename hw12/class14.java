eaa62c1910e57e5cc6e2611884a21853cbb31a5e
rwrewrwer <91109259+rwrewrwer@users.noreply.github.com>
2021-12-22 22:42:25 +0800

    Create class14.java

---
+++ hw12/class14.java
@@ -0,0 +1,10 @@
+
+public class hw12_14 {
+
+	public static void main(String[] args) {
+		String str = "Habit is second nature";
+		System.out.println(str.substring(16,22));//取出位置16~21的字串並印出
+
+	}
+
+}
