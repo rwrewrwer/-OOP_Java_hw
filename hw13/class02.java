5eb7be43efcf0dd44bfe07603cc6f2cb6b5f0f03
rwrewrwer <91109259+rwrewrwer@users.noreply.github.com>
2021-12-24 18:09:48 +0800

    Create class02.java

---
+++ hw13/class02.java
@@ -0,0 +1,16 @@
+public class hw13_2 {
+	public static void main(String argv[])
+	{
+		int num=12 , den = 0;
+		int ans;
+		if(den==0)
+		{
+			System.out.println("除數為0");
+		}
+		else 
+		{
+			ans = num/12;
+		}
+		System.out.println("end of main()!!");
+	}
+}
