b1dc47228d500a4f0c5f471073714b7a42997b42
rwrewrwer <91109259+rwrewrwer@users.noreply.github.com>
2021-12-30 21:58:06 +0800

    Add files via upload

---
+++ hw14/hw14_3.java
@@ -0,0 +1,23 @@
+package hw14_3;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class hw14_3 {
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader buf ;
+		String str;
+		int n;
+		int sum=0;
+		buf = new BufferedReader(new InputStreamReader(System.in));
+		str = buf.readLine();
+		n = Integer.parseInt(str);
+		for(int i=1;i<=n;i++)
+		{
+			sum = sum+i;
+		}
+	System.out.println("sum = "+sum);
+	}
+
+}
