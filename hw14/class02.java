035a1f75da2cb9ee83b7e6d4bc2d154ae750575f
rwrewrwer <91109259+rwrewrwer@users.noreply.github.com>
2021-12-30 21:57:33 +0800

    Create class02.java

---
+++ hw14/class02.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class hw14_2 {
+	public static void main(String argv[])
+	{
+		Scanner sc = new Scanner(System.in);
+		String str ;
+		str = sc.nextLine();
+		System.out.println("大寫 = "+str.toUpperCase());
+	}
+}
